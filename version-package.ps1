44efa165d22b6596388eab3170f0ed7be8a3118a
Larry House <chaos.monkey@hotmail.com>
2016-03-05 06:37:46 -0600

    Setting up nuget package building

---
+++ version-package.ps1
@@ -0,0 +1,10 @@
+param(
+    [string] $version,
+	[string]$branch
+    )
+if($branch -ne "master"){ $version += "-Pre"}
+Write-Host("Building branch '" + $branch + "' as version '" + $version + "'")
+$nuspec = Get-ChildItem "ChaosMonkey.Guards.nuspec" -recurse | Select-Object -First 1
+$content = Get-Content($nuspec)
+$content = $content -replace '\$version\$', $version
+$content | Out-File $nuspec
